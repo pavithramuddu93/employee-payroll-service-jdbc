b18700753d807e3ae319f052f1431a6df1ea68da
pavithramuddu93 <pavithramuddu93>
2022-04-22 20:20:36 +0530

    UC2 added

--- src/main/java/blz/EmployeePayrollDBService.java
+++ src/main/java/blz/EmployeePayrollDBService.java
@@ -1,17 +1,30 @@
 package blz;
-import blz.EmployeePayrollValidation;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeePayrollDBService {
 
+    static EmployeePayrollDBService employeePayrollDBService;
+
+    public EmployeePayrollDBService() {
+
+    }
+
+    public static EmployeePayrollDBService getInstance() {
+        if (employeePayrollDBService == null)
+            employeePayrollDBService = new EmployeePayrollDBService();
+        return employeePayrollDBService;
+    }
+
     /**
      * Method for create connection to the database.
+     *
      * @return : Connection
-     * @throws EmployeePayrollValidation
+     * @throws EmployeePayrollException
      */
-    public Connection getConnection() throws EmployeePayrollValidation {
+    public Connection getConnection() throws EmployeePayrollException {
         Connection con = null;
         try {
             Class.forName("java.sql.DriverManager");
@@ -23,8 +36,37 @@ public class EmployeePayrollDBService {
             con = DriverManager.getConnection(jdbcURL, username, password);
             System.out.println("connection is successfully " + con);
         } catch (Exception e) {
-            throw new EmployeePayrollValidation(e.getMessage());
+            throw new EmployeePayrollException(e.getMessage());
         }
         return con;
+    }
+
+    /**
+     * Method for retrieving data from database.
+     * @return : list of EmployeePayrollData
+     * @throws EmployeePayrollException
+     */
+    public List<EmployeePayrollData> readData() throws EmployeePayrollException {
+
+        String query = "SELECT * FROM employee_payroll; ";
+        List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+
+        try(Connection connection = this.getConnection();) {
+            EmployeePayrollData emData = new EmployeePayrollData();
+            Statement statement =  connection.createStatement();
+            ResultSet result = statement.executeQuery(query);
+            while(result.next()) {
+                emData.setId(result.getInt("id"));
+                emData.setName(result.getString("name"));
+                emData.setGender(result.getString("gender"));
+                emData.setSalary(result.getDouble("salary"));
+                emData.setStartDate(result.getDate("start_date").toLocalDate());
+                employeePayrollList.add(emData);
+            }
+        } catch (SQLException e) {
+            throw new EmployeePayrollException(e.getMessage());
+        }
+        return employeePayrollList;
+
     }
 }
--- src/main/java/blz/EmployeePayrollData.java
+++ src/main/java/blz/EmployeePayrollData.java
@@ -1,14 +1,65 @@
 package blz;
 
-public class EmployeePayrollData {
-    public int id;
-    public String name;
-    public double salary;
 
-    public EmployeePayrollData(int id, String name, double salary) {
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class EmployeePayrollData {
+    private int id;
+    private String name;
+    private  String gender;
+    private double salary;
+    private LocalDate startDate;
+
+    public EmployeePayrollData(){
+
+    }
+    public EmployeePayrollData(int id, String name, String gender, double salary, LocalDate startDate) {
         this.id = id;
         this.name = name;
+        this.gender = gender;
         this.salary = salary;
+        this.startDate = startDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
     }
 
     @Override
@@ -16,7 +67,9 @@ public class EmployeePayrollData {
         return "EmployeePayrollData{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
                 ", salary=" + salary +
+                ", startDate=" + startDate +
                 '}';
     }
 }
