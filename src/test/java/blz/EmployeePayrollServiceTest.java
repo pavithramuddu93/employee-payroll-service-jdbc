5648ed36de8ec151c8346fe29f0c0a69084eb8c6
pavithramuddu93 <pavithramuddu93>
2022-04-23 11:15:11 +0530

    UC6 added

--- src/test/java/blz/EmployeePayrollServiceTest.java
+++ src/test/java/blz/EmployeePayrollServiceTest.java
@@ -6,10 +6,10 @@ import blz.EmployeePayrollData;
 import blz.EmployeePayrollDBService;
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.util.List;
 import java.sql.SQLException;
-
+import java.time.LocalDate;
+import java.util.Map;
 
 public class EmployeePayrollServiceTest {
 
@@ -33,5 +33,21 @@ public class EmployeePayrollServiceTest {
         employeePayrollServiceMain.updateEmployeeSalary("Terisa", 300000.00);
         boolean result = employeePayrollServiceMain.checkEmployeePayrollInSyncWithDB("Terisa");
         Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenDateRange_whenRetrieved_shouldMatchEmployeeCount() throws EmployeePayrollException {
+        employeePayrollServiceMain.retrievingEmployeeData();
+        LocalDate startDate = LocalDate.of(2018,01,01);
+        LocalDate endDate = LocalDate.now();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollServiceMain.readEmployeePayrollForDate(startDate, endDate);
+        Assert.assertEquals(4,employeePayrollData.size());
+    }
+
+    @Test
+    public void givenPayrollData_whenAverageSalaryRetrievedByGender_shouldReturnProperValue() throws EmployeePayrollException {
+        employeePayrollServiceMain.retrievingEmployeeData();
+        Map<String,Double> averageSalaryByGender = employeePayrollServiceMain.readAverageSalaryByGender();
+        Assert.assertTrue(averageSalaryByGender.get("M").equals(50000.00) && averageSalaryByGender.get("F").equals(300000.00));
     }
 }
